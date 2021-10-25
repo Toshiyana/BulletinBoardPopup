fb93d37a17dc4ab888bc4cbd7d87c673f067c15f
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-10-26 03:15:11 +0900

    [add] files

--- BulletinBoardPopup/ViewController.swift
+++ BulletinBoardPopup/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import BLTNBoard
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
+    
+    private lazy var boardManager: BLTNItemManager = {
+        
+        let item = BLTNPageItem(title: "Push Notifications")
+        item.actionButtonTitle = "Continue"
+        item.alternativeButtonTitle = "Maybe Later"
+        item.descriptionText = "Would you like to stay in the loop and get notifications?"
+        
+        // closure
+        item.actionHandler = { _ in
+            self.didTapBoardContinue()
+        }
+        
+        item.alternativeHandler = { _ in
+            self.didTapBoardSkip()
+        }
+        
+        item.appearance.actionButtonColor = .systemGreen
+        item.appearance.alternativeButtonTitleColor = .systemGreen
+        
+        return BLTNItemManager(rootItem: item)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myButton.backgroundColor = .link
+        myButton.setTitleColor(.white, for: .normal)
     }
 
+    @IBAction func didTapButton() {
+        //boardManager.showBulletin(above: self)
+        boardManager.showBulletin(above: self)
+    }
+    
+    func didTapBoardContinue() {
+        print("Did tap continue")
+    }
+    
+    func didTapBoardSkip() {
+        print("Did tap skip")
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,3 @@
+# BullentinBoardPopup-Sample
+* github: https://github.com/alexisakers/BulletinBoard
+* document: https://alexisakers.github.io/BulletinBoard/getting-started.html
